82acbd642b3749615d2e8f6218f673e23a867c6e
elspz <erick.salguero@zemoga.com>
2022-10-28 14:35:49 +0200

    Screenshot implementation
    screenshot and save in a directory

--- src/main/java/org/ultimaHoraHackathon/utils/AndroidActions.java
+++ src/main/java/org/ultimaHoraHackathon/utils/AndroidActions.java
@@ -2,10 +2,21 @@ package org.ultimaHoraHackathon.utils;
 
 import com.google.common.collect.ImmutableMap;
 import io.appium.java_client.AppiumBy;
+import io.appium.java_client.AppiumDriver;
 import io.appium.java_client.android.AndroidDriver;
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.IOUtils;
 import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.remote.RemoteWebElement;
+import sun.misc.BASE64Decoder;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
 
 
 public class AndroidActions extends AppiumUtils{
@@ -36,8 +47,24 @@ public class AndroidActions extends AppiumUtils{
                 "direction", direction,
                 "percent", percent
         ));
+    }
 
+    public void getScreenshotPath(String testCaseName, AppiumDriver driver, String path) throws IOException
+    {
+        String source = ((TakesScreenshot)driver).getScreenshotAs(OutputType.BASE64);
+        String destinationFile = System.getProperty("user.dir")+ path + testCaseName+".png";
+        // Code
+        BASE64Decoder decoder = new BASE64Decoder();
+        byte[] decodedBytes = decoder.decodeBuffer(source);
 
+        File file = new File(destinationFile);;
+        FileOutputStream fop = new FileOutputStream(file);
+
+        fop.write(decodedBytes);
+        fop.flush();
+        fop.close();
+        //FileUtils.copyFile(source, new File("image.png"));
+        //1. capture and place in folder //2. extent report pick file and attach to report
     }
 
 
